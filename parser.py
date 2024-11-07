d80d851208cae11fa94fb545988e2248da07e0f9
Hubert Kasprzycki <67634024+hhubert6@users.noreply.github.com>
2024-11-07 14:41:02 +0100

    Grammar for parser finished (I guess)

--- parser.py
+++ parser.py
@@ -10,8 +10,8 @@ class Mparser(Parser):
         ("nonassoc", 'IFX'),
         ("nonassoc", 'ELSE'),
         ("nonassoc", '<', '>', 'LE', 'GE', 'EQ', 'NEQ'),
-        ("left", '+', '-'),
-        ("left", '*', '/'),
+        ("left", '+', '-', 'DOTADD', 'DOTSUB'),
+        ("left", '*', '/', 'DOTMUL', 'DOTDIV'),
         ("right", 'UMINUS'),
     )
 
@@ -29,47 +29,54 @@ class Mparser(Parser):
        'if_else_instr',
        'print_instr',
        'loop_instr',
-       'block')
+       'block',
+       'return_instr',
+       'BREAK ";"',
+       'CONTINUE ";"')
     def instruction(self, p):
         return p[0]
+
+    # @_('error')
+    # def instruction(self, p):
+    #     return "error"
 
     #
     # ---------- ASSIGNMENT -----------
     #
-    @_('access "="       value ";"',
-       'access ADDASSIGN value ";"',
-       'access SUBASSIGN value ";"',
-       'access MULASSIGN value ";"',
-       'access DIVASSIGN value ";"')
+    @_('ref "="       expr ";"',
+       'ref ADDASSIGN expr ";"',
+       'ref SUBASSIGN expr ";"',
+       'ref MULASSIGN expr ";"',
+       'ref DIVASSIGN expr ";"')
     def assignment(self, p):
-        return (p[1], p.access, p.value)
+        return (p[1], p.ref, p.expr)
 
-    @_('ID', 'ID "[" indexes "]"')
-    def access(self, p):
+    @_('ID')
+    def ref(self, p):
         return p.ID
+
+    @_('ID "[" indexes "]"')
+    def ref(self, p):
+        return ('ref', p.ID, p.indexes)
 
     @_('integer "," indexes')
     def indexes(self, p):
-        pass
+        return [p.integer, *p.indexes]
 
     @_('integer')
     def indexes(self, p):
-        pass
-
-    @_('expr')
-    def value(self, p):
-        return p.expr
+        return [p.integer]
 
     #
     # ----------- IF ELSE INSTRUCTION ------------
     #
     @_('IF "(" expr ")" instruction %prec IFX')
     def if_else_instr(self, p):
-        return ("if", p.expr, p.instruction)
+        return ("if", p.expr, ("then", p.instruction))
 
     @_('IF "(" expr ")" instruction ELSE instruction')
     def if_else_instr(self, p):
-        return ("if", p.expr, p.instruction0, p.instruction1)
+        return ("if", p.expr, ("then", p.instruction0), ("else", p.instruction1))
 
     #
     # ----------- PRINT INSTRUCTION ------------
@@ -77,6 +84,10 @@ class Mparser(Parser):
     @_('PRINT args ";"')
     def print_instr(self, p):
         return ("print", p.args)
+
+    # @_('PRINT error ";"')
+    # def print_instr(self, p):
+    #     return ("print", "error")
 
     @_('arg "," args')
     def args(self, p):
@@ -105,26 +116,37 @@ class Mparser(Parser):
     def integer(self, p):
         return p[0]
 
-
+    #
     # ------- BLOCK --------
+    #
     @_('"{" program "}"')
     def block(self, p):
         return p.program
 
     #
+    # ------- RETURN INSTRUCTION --------
+    #
+    @_('RETURN expr ";"')
+    def return_instr(self, p):
+        return ("return", p.expr)
+
+    #
     # ----------- EXPRESSION -------------
     #
     @_('expr "+" expr',
+       'expr DOTADD expr',
        'expr "-" expr',
+       'expr DOTSUB expr',
        'expr "*" expr',
+       'expr DOTMUL expr',
        'expr "/" expr',
+       'expr DOTDIV expr',
        'expr "<" expr',
        'expr ">" expr',
        'expr LE expr',
        'expr GE expr',
        'expr EQ expr',
-       'expr NEQ expr',
-       )
+       'expr NEQ expr')
     def expr(self, p):
         return (p[1], p.expr0, p.expr1)
 
@@ -136,8 +158,35 @@ class Mparser(Parser):
     def expr(self, p):
         return ("-", p.expr)
 
-    @_('INTNUM', 
+    @_('expr "\'"')
+    def expr(self, p):
+        return ("transpose", p.expr)
+
+    @_('EYE "(" expr ")"', 
+       'ZEROS "(" expr ")"',
+       'ONES "(" expr ")"')
+    def expr(self, p):
+        return (p[0], p.expr)
+
+    @_('vector',
+       'INTNUM', 
        'FLOATNUM', 
        'ID')
     def expr(self, p):
         return p[0]
+
+    #
+    # ---------- VECTOR INITILIZATION ---------
+    #
+    @_('"[" innerlist "]"')
+    def vector(self, p):
+        return ("vector", p.innerlist)
+
+    @_('expr "," innerlist')
+    def innerlist(self, p):
+        return [p.expr, *p.innerlist]
+
+    @_('expr')
+    def innerlist(self, p):
+        return [p.expr]
+
